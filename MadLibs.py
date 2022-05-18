23ec2c9dafbb9c9d1d27a6a4801167a14eb6c4c5
Drdoomsday40 <drdoomsday40@Drdoomsday40Desktop2.localdomain>
2022-05-18 15:26:56 -0700

    first commit

---
+++ MadLibs.py
@@ -0,0 +1,23 @@
+# Provided by: Lucien Maurice
+# Email Address: luciencmaurice@gmail.com
+# FILE: MadLibs.py
+# CLASS PROVIDED: PROJECTCLASSNAME
+#
+# MadLibs.py : this file containes the "main()" function.
+# Program execution begins and ends there.
+#
+# The MadLibs is a class that is designed to ask for nouns, adjectives,
+# verbs, etc and will then add them to a list, set, dictionary, and tuple
+# for showing various ways to use each of those types of data types for 
+# storing collections of data.
+#
+# Member Functions you can call:
+# InsertAtHead() = Play()
+# Next() is going to next song or playing first song in list
+# Prev(), repeat song
+# DoubleClickPrev() is play prev
+# NextRandom() will skip to the next song as random not previously chosen 
+# ShufflePlaylist() all songs in playlist will be shuffled
+# Clear() clears the songs that have been played so far
+# ResetPlaylist() resets the playlist order to original
+
